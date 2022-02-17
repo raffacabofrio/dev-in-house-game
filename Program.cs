a8d5b3be3a3ce139f09e20bca83baf4807ad476b
Raffaello Damgaard Neves <raffaello.neves@pixeon.com>
2022-02-17 20:36:29 -0300

    Base do jogo.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+var heroi = new Personagem{
+    Nome = "Link",
+    Vida = 100,
+    Armadura = 5,
+    Ataque = 5,
+};
+
+var goblim = new Personagem{
+    Nome = "Goblim",
+    Vida = 10,
+    Armadura = 0,
+    Ataque = 0,
+};
+
+
+// tomou um hit
+var dano = 15;
+heroi.Vida = heroi.Vida - dano;
+
+
+Console.WriteLine($"Olá {heroi.Nome}, você tem {heroi.Vida} de vida.");
+
+
+// ganhou uma poção de cura
+heroi.Vida = heroi.Vida + 50;
+
+Console.WriteLine($"Olá {heroi.Nome}, você tem {heroi.Vida} de vida.");
+
+
+public class Personagem {
+    public string Nome { get; set; }
+    public int Vida { get; set; }
+    public int Ataque { get; set; }
+    public int Armadura { get; set; }
+
+}
