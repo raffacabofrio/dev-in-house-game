76d1197f727babc23f5c95a6ae45dc434b483d2a
Johann <johanncf@metalmat.ufrj.br>
2022-02-17 21:57:11 -0300

    Work in progress

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ var heroi = new Personagem{
     Vida = 100,
     Armadura = 5,
     Ataque = 5,
+    AtaqueCritico = 10,
 };
 
 var goblim = new Personagem{
@@ -12,27 +13,62 @@ var goblim = new Personagem{
     Vida = 10,
     Armadura = 0,
     Ataque = 0,
+    AtaqueCritico = 2,
 };
 
+void ataque(Personagem p1, Personagem p2) {
+    int vidaInicialp2 = p2.Vida;
+    
+    Random number = new Random();
+    int resultadoDado = number.Next(11);
 
-// tomou um hit
-var dano = 15;
-heroi.Vida = heroi.Vida - dano;
+    Console.WriteLine($"Resultado do dado: {resultadoDado}");
+
+    int atk;
+
+    if (resultadoDado == 10) {
+        Console.WriteLine($"Wow, {p1.Nome} fez um ataque crítico!");
+        atk = resultadoDado + p1.AtaqueCritico;
+    } else {
+        atk = resultadoDado + p1.Ataque;
+    }
+
+    atk -= p2.Armadura;
+
+    if (atk <= 0) {
+        Console.WriteLine($"{p1.Nome} errou o golpe.");
+        return;
+    }
+
+    p2.Vida -= atk;
+
+    Console.WriteLine($"{p1.Nome} atacou {p2.Nome} com sucesso. {p2.Nome} recebeu {atk} de dano.");
+};
+
+void turno(Personagem p1, Personagem p2) {
+    ataque(p1, p2);
+    if(p2.Vida <= 0) {
+        Console.WriteLine($"{p2.Nome} morreu!");
+        return;
+    } 
+
+    ataque(p2, p1);
+    if(p1.Vida <= 0) {
+        Console.WriteLine($"{p1.Nome} morreu!");
+        return;
+    }
+}
 
 
-Console.WriteLine($"Olá {heroi.Nome}, você tem {heroi.Vida} de vida.");
-
-
-// ganhou uma poção de cura
-heroi.Vida = heroi.Vida + 50;
-
-Console.WriteLine($"Olá {heroi.Nome}, você tem {heroi.Vida} de vida.");
-
+while(heroi.Vida > 0 && goblim.Vida > 0) {
+    turno(heroi, goblim);
+}
 
 public class Personagem {
     public string Nome { get; set; }
     public int Vida { get; set; }
     public int Ataque { get; set; }
     public int Armadura { get; set; }
-
+    public int AtaqueCritico { get; set; }
 }
+
