42c116c7a097a36f55c96f03c88dbaf3c8b98a1b
AndreMotta25 <relaie22@gmail.com>
2022-02-18 11:10:48 -0300

    Criando funcoes num arquivo separado

--- Program.cs
+++ Program.cs
@@ -1,13 +1,15 @@
 ﻿
 
-var heroi = new Personagem{
+var heroi = new Personagem
+{
     Nome = "Link",
     Vida = 100,
     Armadura = 5,
     Ataque = 5,
 };
 
-var goblim = new Personagem{
+var goblim = new Personagem
+{
     Nome = "Goblim",
     Vida = 10,
     Armadura = 0,
@@ -15,23 +17,29 @@ var goblim = new Personagem{
 };
 
 
-void Caminho(int fluxo) {
-    
-    void Direita() {
+void Caminho(int fluxo)
+{
+
+    void Direita()
+    {
         Console.WriteLine("Voce escolheu ir para a direita");
     }
-    void Esquerda() {
+    void Esquerda()
+    {
         Console.WriteLine("Voce escolheu ir para a esquerda");
     }
-    void FlorestaMaligna(){
+    void FlorestaMaligna()
+    {
         Console.WriteLine("Voce escolheu ir pra floresta maligna");
     }
-    void MarBaltico() {
+    void MarBaltico()
+    {
         Console.WriteLine("Voce escolheu ir pra o Mar Baltico");
     }
-    switch(fluxo) {
+    switch (fluxo)
+    {
         case 0:
-            Esquerda(); 
+            Esquerda();
             break;
         case 1:
             Direita();
@@ -51,32 +59,36 @@ void Fluxo(Personagem heroi)
 {
     Console.WriteLine($"Bem vindo Ao jogo {heroi.Nome}");
     Console.ReadKey();
-        var decisao = 3; 
-        bool inputValido2 = false;
-        List<string> caminhos = new List<string>{"Esquerda","Direita","Floresta Maligna","Mar Baltico"};
-        do 
+    var decisao = 3;
+    bool inputValido2 = false;
+    List<string> caminhos = new List<string> { "Esquerda", "Direita", "Floresta Maligna", "Mar Baltico" };
+    do
+    {
+        Console.WriteLine("----------------------");
+        Console.WriteLine($"O que voce deseja fazer {heroi.Nome} ? ");
+        int x = 0;
+        foreach (string opcoes in caminhos)
         {
-            Console.WriteLine("----------------------");
-            Console.WriteLine($"O que voce deseja fazer {heroi.Nome} ? ");
-            int x = 0;
-            foreach(string opcoes in caminhos) {
-                Console.WriteLine($"{opcoes} [{x}]");
-                x++;
-            }
-            var result = Int32.TryParse(Console.ReadLine(), out decisao);
-            if(!inputValido2 && decisao > caminhos.Count ){
-                Console.WriteLine("Voce nao  digitou uma opcao valida!!, Digite novamente");
-            }    
-            else {
-                inputValido2 = true;
-            }
-        }while(!inputValido2);
-        Caminho(decisao);
+            Console.WriteLine($"{opcoes} [{x}]");
+            x++;
+        }
+        var result = Int32.TryParse(Console.ReadLine(), out decisao);
+        if (!inputValido2 && decisao > caminhos.Count)
+        {
+            Console.WriteLine("Voce nao  digitou uma opcao valida!!, Digite novamente");
+        }
+        else
+        {
+            inputValido2 = true;
+        }
+    } while (!inputValido2);
+    Caminho(decisao);
 }
 
 
 
-public class Personagem {
+public class Personagem
+{
     public string Nome { get; set; }
     public int Vida { get; set; }
     public int Ataque { get; set; }
---
+++ funcoes.cs
@@ -0,0 +1,76 @@
+﻿namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            void Caminho(int fluxo)
+            {
+
+                void Direita()
+                {
+                    Console.WriteLine("Voce escolheu ir para a direita");
+                }
+                void Esquerda()
+                {
+                    Console.WriteLine("Voce escolheu ir para a esquerda");
+                }
+                void FlorestaMaligna()
+                {
+                    Console.WriteLine("Voce escolheu ir pra floresta maligna");
+                }
+                void MarBaltico()
+                {
+                    Console.WriteLine("Voce escolheu ir pra o Mar Baltico");
+                }
+                switch (fluxo)
+                {
+                    case 0:
+                        Esquerda();
+                        break;
+                    case 1:
+                        Direita();
+                        break;
+                    case 2:
+                        FlorestaMaligna();
+                        break;
+                    case 3:
+                        MarBaltico();
+                        break;
+                }
+            }
+            void Fluxo(Personagem heroi)
+
+            {
+                Console.WriteLine($"Bem vindo Ao jogo {heroi.Nome}");
+                Console.ReadKey();
+                var decisao = 3;
+                bool inputValido2 = false;
+                List<string> caminhos = new List<string> { "Esquerda", "Direita", "Floresta Maligna", "Mar Baltico" };
+                do
+                {
+                    Console.WriteLine("----------------------");
+                    Console.WriteLine($"O que voce deseja fazer {heroi.Nome} ? ");
+                    int x = 0;
+                    foreach (string opcoes in caminhos)
+                    {
+                        Console.WriteLine($"{opcoes} [{x}]");
+                        x++;
+                    }
+                    var result = Int32.TryParse(Console.ReadLine(), out decisao);
+                    if (!inputValido2 && decisao > caminhos.Count)
+                    {
+                        Console.WriteLine("Voce nao  digitou uma opcao valida!!, Digite novamente");
+                    }
+                    else
+                    {
+                        inputValido2 = true;
+                    }
+                } while (!inputValido2);
+                Caminho(decisao);
+            }
+            // Fluxo(heroi);
+        }
+    }
+}
