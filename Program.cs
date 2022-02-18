a5fd578437957bfcee6bacb34ad639080359624c
AndreMotta25 <relaie22@gmail.com>
2022-02-17 23:02:34 -0300

    criando funcoes

--- Program.cs
+++ Program.cs
@@ -15,6 +15,35 @@ var goblim = new Personagem{
 };
 
 
+void Caminho(int fluxo) {
+    
+    void Direita() {
+        Console.WriteLine("Voce escolheu ir para a direita");
+    }
+    void Esquerda() {
+        Console.WriteLine("Voce escolheu ir para a esquerda");
+    }
+    void FlorestaMaligna(){
+        Console.WriteLine("Voce escolheu ir pra floresta maligna");
+    }
+    void MarBaltico() {
+        Console.WriteLine("Voce escolheu ir pra o Mar Baltico");
+    }
+    switch(fluxo) {
+        case 0:
+            Esquerda(); 
+            break;
+        case 1:
+            Direita();
+            break;
+        case 2:
+            FlorestaMaligna();
+            break;
+        case 3:
+            MarBaltico();
+            break;
+    }
+}
 
 Fluxo(heroi);
 void Fluxo(Personagem heroi)
@@ -22,33 +51,28 @@ void Fluxo(Personagem heroi)
 {
     Console.WriteLine($"Bem vindo Ao jogo {heroi.Nome}");
     Console.ReadKey();
-        // 1 esquerda
-        // 0 direita
         var decisao = 3; 
         bool inputValido2 = false;
+        List<string> caminhos = new List<string>{"Esquerda","Direita","Floresta Maligna","Mar Baltico"};
         do 
         {
-            Console.WriteLine($"O que voce deseja fazer: {heroi.Nome}");
-            Console.WriteLine("Suas opcoes sao ir para esquerda[1] e direita[0]");
+            Console.WriteLine("----------------------");
+            Console.WriteLine($"O que voce deseja fazer {heroi.Nome} ? ");
+            int x = 0;
+            foreach(string opcoes in caminhos) {
+                Console.WriteLine($"{opcoes} [{x}]");
+                x++;
+            }
             var result = Int32.TryParse(Console.ReadLine(), out decisao);
-            if(!inputValido2 && decisao > 1 ){
+            if(!inputValido2 && decisao > caminhos.Count ){
                 Console.WriteLine("Voce nao  digitou uma opcao valida!!, Digite novamente");
             }    
             else {
                 inputValido2 = true;
             }
         }while(!inputValido2);
-
-        
-        if(decisao == 1) {
-            Console.WriteLine("Voce escolheu ir para a esquerda");
-        }
-        else if(decisao == 0){
-            Console.WriteLine("Voce escolheu ir para a direita");
-
-        }
-    }
-    // Console.ReadKey();
+        Caminho(decisao);
+}
 
 
 
