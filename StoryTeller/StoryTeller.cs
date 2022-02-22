79f5d00b71df44342f3352ad7dc1c5bed29dec1f
Eduardo Worrel <eduardo.worrel@gmail.com>
2022-02-21 22:33:00 -0300

    criando classe central

--- StoryTeller/StoryTeller.cs
+++ StoryTeller/StoryTeller.cs
@@ -1,26 +1,40 @@
-public class BattleStoryTeller {
-    private static readonly ConsoleColor BackgroundColor = ConsoleColor.Black; 
-    private static readonly ConsoleColor ForegroundColor  = ConsoleColor.Red; 
-    private static readonly string Narrador = "[Batalha]: ";
-    public static void Speak(string texto, int time = 1000){
-        Console.BackgroundColor = BackgroundColor;
-        Console.ForegroundColor = ForegroundColor;
-        Console.WriteLine(Narrador + texto);
-        Thread.Sleep(time);
-    Console.ForegroundColor  = ConsoleColor.White; 
-        
+public class StoryTeller(){
+
+    public string Narrador;
+
+    public StoryTeller(ConsoleColor BackgroundColor, ConsoleColor ForegroundColor, string narrador){
+
+         Console.BackgroundColor = BackgroundColor;
+         Console.ForegroundColor = ForegroundColor;
+         Narrador = narrador;
     }
+
+    public void Speak((string texto){
+        Console.WriteLine(Narrador + texto);
+        this.Destroy()
+    }
+
+    public void Destroy(){
+        Thread.Sleep(time);
+        Console.ForegroundColor  = ConsoleColor.White; 
+    }
+
+}
+public class BattleStoryTeller : StoryTeller {
+    public BattleStoryTeller(): base(ConsoleColor.Black, ConsoleColor.Red, "[Batalha]: "){}
 }
 public class FlowStoryTeller {
-    private static readonly ConsoleColor BackgroundColor = ConsoleColor.Black; 
-    private static readonly ConsoleColor ForegroundColor  = ConsoleColor.Yellow; 
+     ConsoleColor.Black
+     ConsoleColor.Yellow
+
     private static readonly string Narrador = "[Narrador]: ";
+
     public static void Speak(string texto, int time = 1000){
         Console.BackgroundColor = BackgroundColor;
         Console.ForegroundColor = ForegroundColor;
         Console.WriteLine(Narrador + texto);
         Thread.Sleep(time);
-    Console.ForegroundColor  = ConsoleColor.White; 
+        Console.ForegroundColor  = ConsoleColor.White; 
         
     }
 }
@@ -33,7 +47,7 @@ public class GameStoryTeller {
         Console.ForegroundColor = ForegroundColor;
         Console.WriteLine(Narrador + texto);
         Thread.Sleep(time);
-    Console.ForegroundColor  = ConsoleColor.White; 
+        Console.ForegroundColor  = ConsoleColor.White; 
         
     }
 }
